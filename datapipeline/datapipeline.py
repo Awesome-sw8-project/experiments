c90f2d0768f2ff580e552358b707a63729b1767d
abiram98 <34130705+abiram98@users.noreply.github.com>
2021-03-10 14:36:11 +0100

    appropriate fratures constructed

--- datapipeline/datapipeline.py
+++ datapipeline/datapipeline.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import glob
+
 import os
 import json
 import numpy as np
@@ -7,30 +7,35 @@ import numpy as np
 basepath = ''
 
 subm_file = pd.read_csv('sample_submission.csv')
-subm_df = pd.apply(lambda x: pd.Series(x.split("_")))
+subm_df = subm_file['site_path_timestamp'].apply(lambda x: pd.Series(x.split("_")))
 sites_for_prediction = sorted(subm_df[0].value_counts().index.tolist())
-bssid_for_prediction = list()
-dfs =list()
+bssid_for_prediction = dict()
+dfs = []
 for site in sites_for_prediction:
     #assumes that the data is in a data folder. Possibly needs to explicitly typecast site to string (str())
-    files = [p for p in os.listdir("../data") if p.split('_')[0].startswith(site)]
-    wifi = list()
+    files = [p for p in os.listdir("data") if p.split('_')[0].startswith(site)]
+    #files = [p for p in os.listdir("../data") if p.split('_')[0].startswith(site)]
+    wifi = []
     for file in files:
-        f = open(file)
-        for _,line in enumerate(f):
-            msmt = json.loads(line)
-            if msnt["type"] == "TYPE_wifi":
-                wifi.append(measurement)
-    df = pd.DataFrame(np.array(wifi))
-    val_counts = df[3].value_counts()
-    bssid_for_prediction[site] = val_counts[val_counts > 1000].index.tolist()
-    index = sorted(bssid_for_prediction[site])
-    for _, g in in df.groupby(0):
-        g = g.drop_duplicates(subset=3) #this needs to testing
-        temp = g.iloc[:,3:5]
-        feat = temp.set_index(3).reindex(index).replace(np.nan,-999).T
-        dfs.append(feat)
-    final_df = pd.concat(dfs)
+        f = open("data/"+file,"r")
+        data = json.loads(f.read())
+        f.close()
+        for msm in data["sensorData"]:
+            if msm["Type"] == "TYPE_WIFI":
+                wifi.append(list(msm.values()))
+                
+    if not wifi == []:
+        df = pd.DataFrame(wifi)
+        val_counts = df[3].value_counts() # this step is only necessary if we would like not include all routers/bssid
+        bssid_for_prediction[site] = val_counts.index.tolist()
+        index = sorted(bssid_for_prediction[site])
+        df[0] = df[0].apply(lambda x: int(x))
+        grouped = df.groupby(0)
+        for _, group in grouped:
+            print(group)
+    
+    
+    
 
 
 
