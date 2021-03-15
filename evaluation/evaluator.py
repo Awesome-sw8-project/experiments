7e0d7e61f1ad2172a2c68b8b64f7ffd5ff10f461
MrPekar98 <martin.pekar@outlook.dk>
2021-03-15 18:00:26 +0100

    Initial commit.

--- evaluation/evaluator.py
+++ evaluation/evaluator.py
@@ -1,1 +1,27 @@
+class Evaluator:
+    # Arguments are file names.
+    def __init__(self, evaluation_data, result):
+        self.evaluation_data = evaluation_data
+        self.result = result
 
+    # Entry method for evaluation results.
+    def __eval_results(self):
+        self.__eval_positions()
+        self.__mpe()
+
+    # Evaluates each estimated position.
+    def __eval_positions(self):
+        pass
+
+    # Computes mean positioning error.
+    def __mpe(self):
+        pass
+
+    # Getter to mean positioning error.
+    def get_mpe(self):
+        return None
+
+    # Getter to positioning error for each waypoint.
+    # Return value is map from waypoint to positioning error.
+    def get_pe(self):
+        return None
