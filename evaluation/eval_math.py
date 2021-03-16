e40c0faa626fbd7649dc5ee0f9a3cc13f4652b48
MrPekar98 <martin.pekar@outlook.dk>
2021-03-16 11:53:44 +0100

    No static method.

--- evaluation/eval_math.py
+++ evaluation/eval_math.py
@@ -5,7 +5,6 @@ import math
 # Position error.
 # First argument is estimation list of 3 elements: x, y and floor.
 # Second argument is ground truth list of 3 elements: x, y and floor.
-@staticmethod
 def pe(estimation, ground_truth):
     return math.sqrt(math.pow(estimation[0] - ground_truth[0], 2) + math.pow(estimation[1] - ground_truth[1], 2)) \
              + 15 * math.fabs(estimation[2] -  ground_truth[2])
@@ -13,7 +12,6 @@ def pe(estimation, ground_truth):
 # Mean positopning error.
 # First argument is a list of estimation lists of 3 elements. Inner-most list contains x, y and floor.
 # Second argument is a list of ground truth lists of 3 elements. Inner-most list contains x, y and floor.
-@staticmethod
 def mpe(estimations, ground_truths):
     if len(estimations) != len(ground_truths):
        raise ValueError("Arguments are not of the same size.")
