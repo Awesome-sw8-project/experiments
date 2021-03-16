f36e6a6752176da1c2d89c2849f4c6187304a054
MrPekar98 <martin.pekar@outlook.dk>
2021-03-16 11:09:25 +0100

    Simple evaluation.

--- evaluation/eval_math.py
+++ evaluation/eval_math.py
@@ -2,30 +2,25 @@ import math
 
 # Container of positioning error calculations.
 class Eval:
-    # Positioning error.
+    # Position error.
+    # First argument is estimation list of 3 elements: x, y and floor.
+    # Second argument is ground truth list of 3 elements: x, y and floor.
     @staticmethod
-    def pe(x_pred, y_pred, x_act, y_act, floor_pred, floor_act):
-        return math.sqrt(math.pow(x_pred - x_act, 2) + math.pow(y_pred, y_act, 2)) + 15 * math.fabs(floor_pred - floor_act)
+    def pe(estimation, ground_truth):
+        return math.sqrt(math.pow(estimation[0] - ground_truth[0], 2) + math.pow(estimation[1] - ground_truth[1], 2)) \
+                 + 15 * math.fabs(estimation[2] -  ground_truth[2])
 
-    # Mean positioning error.
+    # Mean positopning error.
+    # First argument is a list of estimation lists of 3 elements. Inner-most list contains x, y and floor.
+    # Second argument is a list of ground truth lists of 3 elements. Inner-most list contains x, y and floor.
     @staticmethod
-    def mpe(x_preds, y_preds, x_acts, y_acts, floor_preds, floor_acts):
-        if Eval.__check_sizes(x_preds, y_preds, x_acts, y_acts, floor_preds, floor_acts)
+    def mpe(estimations, ground_truths):
+        if len(estimations) != len(ground_truths):
             raise ValueError("Arguments are not of the same size.")
 
         sum = 0
 
-        for i in range(len(x_preds)):
-            sum += Eval.pe(x_preds[i], y_preds[i], x_acts[i], y_acts[i], floor_preds[i], floor_acts[i])
+        for i in range(len(estimations)):
+            sum += Eval.pe(estimations[i], ground_truths[i])
 
-        return sum / len(x_preds)
-
-    # Checks MPE list values are of the same size.
-    @staticmethod
-    def __check_sizes(x_preds, y_preds, x_acts, y_acts, floor_preds, floor_acts):
-        s = len(x_preds)
-
-        if len(y_preds) != s or len(x_acts) != s or len(y_acts) != s or len(floor_preds) != s or len(floor_acts) != s:
-            return False
-
-        return True
+        return sum / len(estimations)
--- evaluation/evaluator.py
+++ evaluation/evaluator.py
@@ -1,10 +1,16 @@
-import eval_math
+import eval_math as em
 
 class Evaluator:
     # Arguments are file names.
-    def __init__(self, evaluation_data, result):
+    def __init__(self, evaluation_data, actual):
+        if len(evaluation_data) != len(actual):
+            raise ValueError("Arguments are not of the same size.")
+
         self.evaluation_data = evaluation_data
-        self.result = result
+        self.ground_truth_data = actual
+        self.mpe = 0
+        self.pe = []
+        self.__eval_results()
 
     # Entry method for evaluation results.
     def __eval_results(self):
@@ -13,17 +19,17 @@ class Evaluator:
 
     # Evaluates each estimated position.
     def __eval_positions(self):
-        pass
+        for i in range(len(self.evaluation_data)):
+            self.pe.append(em.Eval.pe(self.evaluation_data[i], self.ground_truth_data[i]))
 
     # Computes mean positioning error.
     def __mpe(self):
-        pass
+        self.mpe = em.Eval.mpe(self.evaluation_data, self.ground_truth_data)
 
     # Getter to mean positioning error.
     def get_mpe(self):
-        return None
+        return self.mpe
 
-    # Getter to positioning error for each waypoint.
-    # Return value is map from waypoint to positioning error.
+    # Getter to positioning error for each estimation.
     def get_pe(self):
-        return None
+        return self.pe
