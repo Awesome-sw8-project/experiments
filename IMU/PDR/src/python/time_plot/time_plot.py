2090316006ccd491b30bd91c8305cf39a7525ffe
MrPekar98 <martin.pekar@outlook.dk>
2021-04-11 22:40:54 +0200

    Time difference plotting.

---
+++ IMU/PDR/src/python/time_plot/time_plot.py
@@ -0,0 +1,28 @@
+import csv
+import sys
+sys.path.append("../../../../..")
+import datapipeline.datapipeline as dp
+
+iterator = dp.imu_data("../../../../../../data/data/train/", "../../../../../../data/data/sample_submission.csv", 0)
+prev_time = None
+
+for data in iterator:
+    i = 0
+
+    with open(data[0] + ".csv", "w", newline = '') as file:
+        writer = csv.writer(file)
+        prev_time = data[1][0]
+        writer.writerow(["i", "Time difference"])
+
+        for measure in data[1]:
+            timestamp = measure[0]
+
+            if i == 0:
+                prev_time = timestamp
+                writer.writerow([str(i), "0"])
+                i += 1
+                continue
+
+            writer.writerow([str(i), str(float((timestamp - prev_time)) / 1000)])
+            prev_time = timestamp
+            i += 1
