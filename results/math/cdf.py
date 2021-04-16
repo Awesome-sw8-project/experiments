0c8f5f028c20920a9bb3131694a6d942a0b42036
MrPekar98 <martin.pekar@outlook.dk>
2021-04-16 16:04:21 +0200

    CDF function.

---
+++ results/math/cdf.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Main entry function for CDF plotting.
+def plot(data, size, title, x_label):
+    x = np.sort(data)
+    y = np.arange(size) / float(size)
+
+    plt.xlabel(x_label)
+    plt.ylabel("Distribution")
+    plt.title(title)
+    plt.plot(x, y, label = 'CDF', marker = 'o')
+    plt.savefig("./test.png")
+    plt.close()
