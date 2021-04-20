c535cf069c36eecd50ed792fdf2f2d3cd9a35db3
mijens17 <mijens17@student.aau.dk@nv-ai-01.srv.aau.dk>
2021-04-20 09:21:47 +0200

    Logging.

--- results/PDR/time_plotting.py
+++ results/PDR/time_plotting.py
@@ -6,14 +6,20 @@ import os
 # Composes all .csv files into one .csv file.
 # Ignores headers assuming all files have a header.
 # It also assumes comma-separation.
-def compose_csv(header, dir, out_name):
+def compose_csv(header, dir, out_name, log_progress):
     files = [p for p in os.listdir(dir) if p.endswith(".csv")]
+    count = 0
 
     with open(out_name,  "w", newline = '') as out_file:
         writer = csv.writer(out_file)
         writer.writerow(header)
 
         for file in files:
+            count += 1
+
+            if log_progress:
+                print("Composing " + str(int((count / len(files)) * 100)) + "%...", end = '\r', flush = True)
+
             with open(dir + "/" + file, "r") as in_file:
                 reader = csv.reader(in_file, delimiter = ',')
                 start = True
@@ -26,6 +32,6 @@ def compose_csv(header, dir, out_name):
                     writer.writerow(line)
 
 if __name__ == "__main__":
-    compose_csv(["i", "Time difference"], "timing", "composed.csv")
+    compose_csv(["i", "Time difference"], "timing", "composed.csv", True)
     timings = pd.read_csv("composed.csv")
     boxplot = data.boxplot(column = ["Time difference"])
