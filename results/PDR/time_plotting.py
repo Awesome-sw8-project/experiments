191f48ca33d995c516b6d2753c146d58397ef120
MrPekar <martin.pekar@outlook.dk>
2021-04-26 11:05:19 +0200

    Bug fixes.

--- results/PDR/time_plotting.py
+++ results/PDR/time_plotting.py
@@ -5,12 +5,19 @@ import os
 import matplotlib.pyplot as plt
 import sys
 sys.path.append("..")
-import math.cdf as cdf
+import plot.cdf as c
+
+# Enables using multi-character delimiter.
+def multi_delimiter(delimiter):
+    if (len(delimiter) == 1):
+        return delimiter
+
+    return delimiter[-1]
 
 # Composes all .csv files into one .csv file.
 # Ignores headers assuming all files have a header.
-# It also assumes comma-separation.
-def compose_csv(header, dir, file_type, delimiter, out_name, log_progress):
+# It also assumes comma-separation and exactly two columns.
+def compose_csv(header, dir, file_type, delimiter, out_name, log_progress, skip_lines):
     files = [p for p in os.listdir(dir) if p.endswith("." + file_type)]
     count = 0
 
@@ -22,23 +29,19 @@ def compose_csv(header, dir, file_type, delimiter, out_name, log_progress):
             count += 1
 
             if log_progress:
-                print("Composing " + str(int((count / len(files)) * 100)) + "%...", end = '\r', flush = True)
+                print("Composing into " + out_name + ": " + str(int((count / len(files)) * 100)) + "%", end = '\r', flush = True)
 
             with open(dir + "/" + file, "r") as in_file:
-                reader = csv.reader(in_file, delimiter = delimiter)
-                start = True
-
+                reader = csv.reader(in_file, delimiter = multi_delimiter(delimiter))
+                
                 for i, line in enumerate(reader):
-                    if start:
-                        start = False
-                        continue
-
-                    writer.writerow(line)
+                    if i > skip_lines:
+                        writer.writerow([str(line[0]), str(line[-1])])
 
 if __name__ == "__main__":
     compose_csv(["i", "Time difference"], "timing", "csv", ',', "composed.csv", True)
-    compose_csc(["Measurement", "PE"], "madgwick", "csv", '\t\t', "madgwick_pes.csv", True)
-    compose_csv(["Measurement", "PE"], "kalman", "csv", '\t\t', "kalman_pes.csv", True)
+    compose_csv(["Measurement", "PE"], "madgwick/", "txt", '\t\t', "madgwick_pes.csv", True, 3)
+    compose_csv(["Measurement", "PE"], "kalman/", "txt", '\t\t', "kalman_pes.csv", True, 3)
 
     timings = pd.read_csv("composed.csv")
     plt.boxplot(list(timings['Time difference']), labels = ['Time difference'], sym = '', autorange = True, patch_artist = True)
@@ -48,8 +51,8 @@ if __name__ == "__main__":
     kalman_pes = pd.read_csv("kalman_pes.csv")
     madgwick_pe_list = list(madgwick_pes['PE'])
     kalman_pe_list = list(kalman_pes['PE'])
-    cdf.plot(madgwick_pe_list, len(madgwick_pe_list), "Cumulative Distribution Function of Positioning Errors (Madgwick)", "Positioning error (M)")
-    cdf.plot(kalman_pe_list, len(kalman_pe_list), "Cumulative Distribution Function of Positioning Errors (Extended Kalman)", "Positioning error (M)")
+    c.plot(madgwick_pe_list, len(madgwick_pe_list), "Cumulative Distribution Function of Positioning Errors (Madgwick)", "Positioning error (M)")
+    c.plot(kalman_pe_list, len(kalman_pe_list), "Cumulative Distribution Function of Positioning Errors (Extended Kalman)", "Positioning error (M)")
 
     os.remove("composed.csv")
     os.remove("madgwick_pes.csv")
