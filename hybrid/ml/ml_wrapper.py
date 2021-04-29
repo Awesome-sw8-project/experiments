6dc160354854d7f6446a6f38a8593d0094faeb3a
MrPekar <martin.pekar@outlook.dk>
2021-04-29 15:31:11 +0200

    Main ML wrapper architecture

---
+++ hybrid/ml/ml_wrapper.py
@@ -0,0 +1,33 @@
+# Position estimation interface.
+class Estimator:
+    def next_position(self, data):
+        pass
+
+# Wrapper of ML algorithms used in this project.
+class MLWrapper(Estimator):
+    def __init__(self, ml_algorithm, algorithm_label):
+        self.algorithm = ml_algorithm
+        self.label = algorithm_label
+
+    # Estimates next position.
+    def next_position(self, data):
+        if self.algorithm.lower() == "ann":
+            return self.__ann_next_position(data)
+
+        elif self.algorithm.lower() == "rnn"
+            return self.__rnn_next_position(data)
+
+        elif self.algorithm.lower() == "knn":
+            return self.__knn_next_position(data)
+
+    # ANN next position estimation.
+    def __ann_next_position(self, data):
+        pass
+
+    # RNN next position estimation.
+    def __rnn_next_position(self, data):
+        pass
+
+    # KNN next position estimation.
+    def __knn_next_position(self, data):
+        pass
