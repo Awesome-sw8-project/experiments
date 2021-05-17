91e2aeb53ff4be9e21162aa660750476a47977bf
Mikkel Filip Jensen <mijens17@student.aau.dk>
2021-05-17 14:15:29 +0200

    Temp code

--- ML/LightGBM.py
+++ ML/LightGBM.py
@@ -24,7 +24,7 @@ from load_data import *
 # ------------------------------------------------------------------------------
 # Fixed values
 # ------------------------------------------------------------------------------
-N_SPLITS = 20
+N_SPLITS = 5
 SEED = 42
 
 # ------------------------------------------------------------------------------
@@ -32,7 +32,6 @@ SEED = 42
 # ------------------------------------------------------------------------------
 LOG_PATH = Path("./log/")
 LOG_PATH.mkdir(parents=True, exist_ok=True)
-
 
 # ------------------------------------------------------------------------------
 # Utilities
@@ -70,14 +69,22 @@ def score_log(df: pd.DataFrame, num_files: int, nam_file: str, data_shape: tuple
     df.to_csv(LOG_PATH / f"log_score.csv", index=False)
     return df
 
+def create_submission_file(all_preds):
+    all_preds = pd.concat(predictions)
+    all_preds = all_preds.reindex(subm.index)
+    all_preds.to_csv('submission.csv')
 
 # ------------------------------------------------------------------------------
 # Set seed
 # ------------------------------------------------------------------------------
 set_seed(SEED)
-train_files = sorted(glob.glob('../input/indoor-positioning-traindata/train_data/*'))
-test_files = sorted(glob.glob('../input/indoor-positioning-traindata/test_data/*'))
-subm = pd.read_csv('../input/indoor-positioning-traindata/sample_submission.csv', index_col=0)
+
+# ------------------------------------------------------------------------------
+# Input data
+# ------------------------------------------------------------------------------
+train_files = sorted(glob.glob('../input/indoor-positioning-data/train_data/*'))
+test_files = sorted(glob.glob('../input/indoor-positioning-data/test_data/*'))
+subm = pd.read_csv('../input/indoor-positioning-data/sample_submission.csv', index_col=0)
 
 # ------------------------------------------------------------------------------
 # Define parameters for models
@@ -88,7 +95,7 @@ lgb_params = {'objective': 'root_mean_squared_error',
               'learning_rate': 0.1,
               'num_leaves': 90,
               'colsample_bytree': 0.4,
-              'subsample': 0.6,
+              'subsample': 0.7,
               'subsample_freq': 2,
               'bagging_seed': SEED,
               'reg_alpha': 8,
@@ -112,150 +119,116 @@ lgb_f_params = {'objective': 'multiclass',
                 'n_jobs': -1
                 }
 
+if __name__ == '__main__':
+    
+    # ------------------------------------------------------------------------------
+    # Training and inference
+    # ------------------------------------------------------------------------------
+    score_df = pd.DataFrame()
+    oof = list()
+    predictions = list()
 
-# ------------------------------------------------------------------------------
-# Training and inference
-# ------------------------------------------------------------------------------
-score_df = pd.DataFrame()
-oof = list()
-predictions = list()
+    for n_files, file in enumerate(train_files):
+        # TRAIN
+        site, train, ground = gen_for_serialisation_one_path(file)
+        df1 = pd.DataFrame(train)
+        df2 = pd.DataFrame(ground)
+        data = pd.concat([df1, df2], axis=1, join='inner')
 
-for n_files, file in enumerate(train_files):
-    # TRAIN
-    site, train, ground = gen_for_serialisation_one_path(file)
-    df1 = pd.DataFrame(train)
-    df2 = pd.DataFrame(ground)
-    #df2.columns = ['x','y','f']
-    data = pd.concat([df1, df2], axis=1, join='inner')
+        # TEST
+        test_data = gen_for_serialisation_one_path_test(test_files[n_files])
+        test_data = pd.DataFrame(test_data)
+        test_data.columns = ['Bssid','site_path_timestamp']
+        test_data = pd.concat([pd.DataFrame(i for i in test_data['Bssid']).astype(int),test_data['site_path_timestamp']], axis=1)
 
-    # TEST
-    test_data = gen_for_serialisation_one_path_test(test_files[n_files])
-    test_data = pd.DataFrame(test_data)
-    test_data.columns = ['Bssid','site_path_timestamp']
-    test_data = pd.concat([pd.DataFrame(i for i in test_data['Bssid']).astype(int),test_data['site_path_timestamp']], axis=1)
-    #print(test_data.iloc[:, :-1].astype(int))
-
-    oof_x, oof_y, oof_f = np.zeros(data.shape[0]), np.zeros(data.shape[0]), np.zeros(data.shape[0])
-    preds_x, preds_y = 0, 0
-    preds_f_arr = np.zeros((test_data.shape[0], N_SPLITS))
-
-
-    kf = KFold(n_splits=N_SPLITS, shuffle=True, random_state=SEED)
-    for fold, (trn_idx, val_idx) in enumerate(kf.split(data.iloc[:, :-3])):
-        X_train = data.iloc[trn_idx, :-3].astype(int)
-        y_trainx = data.iloc[trn_idx, -3]
-        y_trainy = data.iloc[trn_idx, -2]
-        y_trainf = data.iloc[trn_idx, -1]
-
-        X_valid = data.iloc[val_idx, :-3].astype(int)
-        y_validx = data.iloc[val_idx, -3]
-        y_validy = data.iloc[val_idx, -2]
-        y_validf = data.iloc[val_idx, -1]
+        oof_x, oof_y, oof_f = np.zeros(data.shape[0]), np.zeros(data.shape[0]), np.zeros(data.shape[0])
+        preds_x, preds_y = 0, 0
+        preds_f_arr = np.zeros((test_data.shape[0], N_SPLITS))
 
 
-        modelx = lgb.LGBMRegressor(**lgb_params)
-        with timer("fit X"):
-            modelx.fit(X_train, y_trainx,
-                       eval_set=[(X_valid, y_validx)],
-                       eval_metric='rmse',
-                       verbose=False,
-                       early_stopping_rounds=20
-                       )
+        kf = KFold(n_splits=N_SPLITS, shuffle=True, random_state=SEED)
+        for fold, (trn_idx, val_idx) in enumerate(kf.split(data.iloc[:, :-3])):
+            X_train = data.iloc[trn_idx, :-3].astype(int)
+            y_trainx = data.iloc[trn_idx, -3]
+            y_trainy = data.iloc[trn_idx, -2]
+            y_trainf = data.iloc[trn_idx, -1]
 
-        modely = lgb.LGBMRegressor(**lgb_params)
-        with timer("fit Y"):
-            modely.fit(X_train, y_trainy,
-                       eval_set=[(X_valid, y_validy)],
-                       eval_metric='rmse',
-                       verbose=False,
-                       early_stopping_rounds=20
-                       )
+            X_valid = data.iloc[val_idx, :-3].astype(int)
+            y_validx = data.iloc[val_idx, -3]
+            y_validy = data.iloc[val_idx, -2]
+            y_validf = data.iloc[val_idx, -1]
 
-        modelf = lgb.LGBMClassifier(**lgb_f_params)
-        with timer("fit F"):
-            modelf.fit(X_train, y_trainf,
-                       eval_set=[(X_valid, y_validf)],
-                       eval_metric='multi_logloss',
-                       verbose=False,
-                       early_stopping_rounds=20
-                       )
+            if not(os.path.exists(f'LightGBM_models/{n_files}modelx.txt')):
+                modelx = lgb.LGBMRegressor(**lgb_params)
+                with timer("fit X"):
+                    modelx.fit(X_train, y_trainx,
+                               eval_set=[(X_valid, y_validx)],
+                               eval_metric='rmse',
+                               verbose=False,
+                               early_stopping_rounds=20
+                               )
+                    
+                modelx.booster_.save_model(f'LightGBM_models/{n_files}modelx.txt')
+            else:
+                modelx = lgb.Booster(model_file=f'LightGBM_models/{n_files}modelx.txt')
 
-        oof_x[val_idx] = modelx.predict(X_valid)
-        oof_y[val_idx] = modely.predict(X_valid)
-        oof_f[val_idx] = modelf.predict(X_valid).astype(int)
+            if not(os.path.exists(f'LightGBM_models/{n_files}modely.txt')):
+                modely = lgb.LGBMRegressor(**lgb_params)
+                with timer("fit Y"):
+                    modely.fit(X_train, y_trainy,
+                               eval_set=[(X_valid, y_validy)],
+                               eval_metric='rmse',
+                               verbose=False,
+                               early_stopping_rounds=20
+                               )
+                    
+                modely.booster_.save_model(f'LightGBM_models/{n_files}modely.txt')
+            else:
+                modely = lgb.Booster(model_file=f'LightGBM_models/{n_files}modely.txt')
 
-        preds_x += modelx.predict(test_data.iloc[:, :-1]) / N_SPLITS
-        preds_y += modely.predict(test_data.iloc[:, :-1]) / N_SPLITS
-        preds_f_arr[:, fold] = modelf.predict(test_data.iloc[:, :-1]).astype(int)
+            if not(os.path.exists(f'LightGBM_models/{n_files}modelf.txt')):
+                modelf = lgb.LGBMClassifier(**lgb_f_params)
+                with timer("fit F"):
+                    modelf.fit(X_train, y_trainf,
+                               eval_set=[(X_valid, y_validf)],
+                               eval_metric='multi_logloss',
+                               verbose=False,
+                               early_stopping_rounds=20
+                               )
+                    
+                modelf.booster_.save_model(f'LightGBM_models/{n_files}modelf.txt')
+            else:
+                modelf = lgb.Booster(model_file=f'LightGBM_models/{n_files}modelf.txt')
 
-        score = comp_metric(oof_x[val_idx], oof_y[val_idx], oof_f[val_idx],
-                            y_validx.to_numpy(), y_validy.to_numpy(), y_validf.to_numpy())
-        print(f"fold {fold}: mean position error {score}")
-        score_df = score_log(score_df, n_files, os.path.basename(file), data.shape, fold, SEED, score)
+            oof_x[val_idx] = modelx.predict(X_valid)
+            oof_y[val_idx] = modely.predict(X_valid)
+            oof_f[val_idx] = modelf.predict(X_valid).astype(int)
 
+            preds_x += modelx.predict(test_data.iloc[:, :-1]) / N_SPLITS
+            preds_y += modely.predict(test_data.iloc[:, :-1]) / N_SPLITS
+            preds_f_arr[:, fold] = modelf.predict(test_data.iloc[:, :-1]).astype(int)
 
-    print("*+"*40)
-    print(f"file #{n_files}, shape={data.shape}, name={os.path.basename(file)}")
-    score = comp_metric(oof_x, oof_y, oof_f,
-                        data.iloc[:, -3].to_numpy(), data.iloc[:, -2].to_numpy(), data.iloc[:, -1].to_numpy())
-    oof.append(score)
-    print(f"mean position error {score}")
-    print("*+"*40)
-    score_df = score_log(score_df, n_files, os.path.basename(file), data.shape, 999, SEED, score)
-
-    preds_f_mode = stats.mode(preds_f_arr, axis=1)
-    preds_f = preds_f_mode[0].astype(int).reshape(-1)
-    test_preds = pd.DataFrame(np.stack((preds_f, preds_x, preds_y))).T
-    test_preds.columns = subm.columns
-    test_preds.index = test_data["site_path_timestamp"]
-    test_preds["floor"] = test_preds["floor"].astype(int)
-    predictions.append(test_preds)
-    print(test_preds)
-
-
-
-# for site, train, truth in gen-train:
-#     fit_model_site(site, train, truth)
-
-# create file with results
-all_preds = pd.concat(predictions)
-all_preds = all_preds.reindex(subm.index)
-all_preds.to_csv('submission.csv')
+            score = comp_metric(oof_x[val_idx], oof_y[val_idx], oof_f[val_idx],
+                                y_validx.to_numpy(), y_validy.to_numpy(), y_validf.to_numpy())
+            print(f"fold {fold}: mean position error {score}")
+            score_df = score_log(score_df, n_files, os.path.basename(file), data.shape, fold, SEED, score)
 
 
+        print("*+"*40)
+        print(f"file #{n_files}, shape={data.shape}, name={os.path.basename(file)}")
+        score = comp_metric(oof_x, oof_y, oof_f,
+                            data.iloc[:, -3].to_numpy(), data.iloc[:, -2].to_numpy(), data.iloc[:, -1].to_numpy())
+        oof.append(score)
+        print(f"mean position error {score}")
+        print("*+"*40)
+        score_df = score_log(score_df, n_files, os.path.basename(file), data.shape, 999, SEED, score)
 
-## Needed in load_data.py for this to work!
-#
-#
-# import os, pickle
+        preds_f_mode = stats.mode(preds_f_arr, axis=1)
+        preds_f = preds_f_mode[0].astype(int).reshape(-1)
+        test_preds = pd.DataFrame(np.stack((preds_f, preds_x, preds_y))).T
+        test_preds.columns = subm.columns
+        test_preds.index = test_data["site_path_timestamp"]
+        test_preds["floor"] = test_preds["floor"].astype(int)
+        predictions.append(test_preds)
 
-# def get_x_y_floor(truth_array):
-#     xs = list()
-#     ys = list()
-#     floors = list()
-#     for lst in truth_array:
-#         xs.append(lst[0])
-#         ys.append(lst[1])
-#         floors.append(lst[2])
-#     return xs,ys,floors
-
-# def gen_for_serialisation(path_to_train):
-#     site_data_files = [x for x in os.listdir(path_to_train)]
-#     for file in site_data_files:
-#         #../input/indoor-positioning-traindata/train_data
-#         f = open(path_to_train +'/'+file, "rb")
-#         site, train, ground = pickle.load(f)
-#         f.close()
-#         yield site,train,ground
-
-# def gen_for_serialisation_one_path(path_to_train):
-#         f = open(path_to_train, "rb")
-#         site, train, ground = pickle.load(f)
-#         f.close()
-#         return site,train,ground
-
-# def gen_for_serialisation_one_path_test(path_to_train):
-#         f = open(path_to_train, "rb")
-#         test = pickle.load(f)
-#         f.close()
-#         return test
+    create_submission_file(all_preds)
