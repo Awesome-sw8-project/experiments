f240e775f41fc3b8b6070d016eb551f7284e3dc5
Mikkel Filip Jensen <mikkelfilip1@gmail.com>
2021-05-21 20:29:55 +0200

    Update LightGBM.py

--- ML/LightGBM.py
+++ ML/LightGBM.py
@@ -21,44 +21,7 @@ import math
 from contextlib import contextmanager
 import joblib
 
-import os, pickle
 
-def get_x_y_floor(truth_array):
-    xs = list()
-    ys = list()
-    floors = list()
-    for lst in truth_array:
-        xs.append(lst[0])
-        ys.append(lst[1])
-        floors.append(lst[2])
-    return xs,ys,floors
-
-def gen_for_serialisation(path_to_train):
-    site_data_files = [x for x in os.listdir(path_to_train)]
-    for file in site_data_files:
-        #../input/indoor-positioning-traindata/train_data
-        f = open(path_to_train +'/'+file, "rb")
-        site, train, ground = pickle.load(f)
-        f.close()
-        yield site,train,ground
-
-def get_data_for_test(pt_test,site):
-    test_data = None
-    with open("{pt_test}/{site}.pickle".format(pt_test=pt_test,site=site), "rb") as f:
-        test_data = pickle.load(f)
-    return test_data
-
-def gen_for_serialisation_one_path(path_to_train):
-        f = open(path_to_train, "rb")
-        site, train, ground = pickle.load(f)
-        f.close()
-        return site,train,ground
-
-def gen_for_serialisation_one_path_test(path_to_train):
-        f = open(path_to_train, "rb")
-        test = pickle.load(f)
-        f.close()
-        return test
         
 # ------------------------------------------------------------------------------
 # Fixed values
