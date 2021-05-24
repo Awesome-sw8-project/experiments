f9842844ca212dd11f7704bb5adcb0bcd3ca8082
Ubuntu <martin.pekar@outlook.dk>
2021-05-24 21:45:34 +0200

    Latest hybrid implementation

--- hybrid/hybrid.py
+++ hybrid/hybrid.py
@@ -6,8 +6,6 @@ sys.path.append("../IMU/PDR/src/python")
 import pdr.pdr as p
 sys.path.append("..")
 import evaluation.evaluator as evaluator, evaluation.eval_output as eo
-sys.path.append("..")
-import datapipeline.datapipeline as pipe
 
 model_map = {
     "5da138754db8ce0c98bca82f": 1,
@@ -22,9 +20,9 @@ def gen_for_serialisation(path_to_train):
     site_data_files = [x for x in os.listdir(path_to_train)]
     for file in site_data_files:
         f = open(path_to_train +'/'+file, "rb")
-        site, site_data = pickle.load(f)
+        site_data = pickle.load(f)
         f.close()
-        yield site, site_data
+        yield file.split('.')[0], site_data
 
 # Base class of hybrid position estimators.
 class Hybrid(ml.Estimator):
@@ -54,10 +52,14 @@ class MLPDRHybrid(Hybrid):
 
     # Main entry for estimating position.
     def next_position(self, imu_data, antenna_data):
-        pos = self.ml.next_position(antenna_data)
+        pos = []
+
+        if antenna_data != []:
+            pos = self.ml.next_position(antenna_data)
+
         timestamp = imu_data[0]
 
-        if (timestamp - self.prev_timestamp > self.time_limit or antenna_data == []):
+        if (timestamp - self.prev_timestamp > self.time_limit or (len(pos) == 0)):
             if (not self.using_pdr):
                 self.pdr_recalibrate(p.Location(self.last_position[0], self.last_position[1]))
                 self.using_pdr = True
@@ -84,22 +86,26 @@ class PDRMLHybrid(Hybrid):
     def __init__(self, start_location, algorithm_label):
         super().__init__(start_location, algorithm_label)
         self.pdr_measurement_count = 0
-        self.recal_limit = 350
-        self.latest_ml_pos = [0, 0, 0]
+        self.recal_limit = 200
+        self.latest_ml_pos = start_location
 
     # Main entry for estimating position.
     # Also estimates position using ML and stores it in class member, since we might not always
     # get a position estimation from the ML algorithm.
     def next_position(self, imu_data, antenna_data):
         pdr_pos = self.pdr.get_current_location(imu_data[0], imu_data[1], imu_data[2], imu_data[3])
-        ml_pos = self.ml.next_position(antenna_data)
+        ml_pos = None
+
+        if len(antenna_data) > 0:
+            ml_pos = self.ml.next_position(antenna_data)
+
         self.pdr_measurement_count += 1
 
         if (ml_pos != None):
             self.latest_ml_pos = ml_pos
 
-        if (self.measurement_count >= self.recal_limit):
-            self.pdr_recalibrate(self.latest_ml_pos)
+        if (self.pdr_measurement_count >= self.recal_limit):
+            self.pdr_recalibrate(p.Location(self.latest_ml_pos[0], self.latest_ml_pos[1]))
 
         return [pdr_pos.get_x(), pdr_pos.get_y(), self.latest_ml_pos[2]]
 
@@ -119,7 +125,11 @@ class AverageHybrid(Hybrid):
     # Outout is on form [<X>, <Y>, <Z>].
     def next_position(self, imu_data, antenna_data):
         self.estimate_count += 1
-        ml_pos = self.ml.next_position(antenna_data)
+        ml_pos = None
+
+        if len(antenna_data) > 0:
+            ml_pos = self.ml.next_position(antenna_data)
+
         pdr_pos = self.pdr.get_current_location(imu_data[0], imu_data[1], imu_data[2], imu_data[3])
         avg_x = pdr_pos.get_x()
         avg_y = pdr_pos.get_y()
@@ -141,45 +151,56 @@ class AverageHybrid(Hybrid):
     def set_model_index(self, index):
         self.ml.set_model_index(index)
 
+def get_imu_data(site, path_to_imu):
+    with open("{}/{}.pickle".format(path_to_imu,site), "rb") as f:
+        data = pickle.load(f)
+    return data
+
 if __name__ == "__main__":
-    iter = gen_for_serialisation("../../data/data/filtered")
+    rssi_iter = gen_for_serialisation("../../data/data/filtered_rssi")
+    rssi_lst = []
     site_count = 0
 
-    for site in iter:
-        if site_count < 0:
-            site_count += 1
-            continue
-
-        ground_truths = list()
+    for site, rssi_data in rssi_iter:
+        imu_data = get_imu_data(site, "../../data/data/filtered_imu")
+        paths = list(imu_data.keys())
+        ground_truth = list()
         estimations = list()
 
-        for path_data in site[1]:
-            time_mapping = path_data[2]
-            keys = list(time_mapping.keys())
-            hybrid = MLPDRHybrid(time_mapping[keys[0]][0], "lightgbm")
-            hybrid.set_model_index(model_map[site[0]])
+        for path in paths:
+            timestamps = list(rssi_data[path].keys())
+            timestamps.extend(list(imu_data[path].keys()))
+            timestamps = list(set(timestamps))
+            timestamps = [int(x) for x in timestamps]
+            timestamps = sorted(timestamps)
+            time_to_rssi = rssi_data[path]
+            time_to_imu = imu_data[path]
+            hybrid = AverageHybrid(time_to_imu[str(timestamps[0])][4], "lightgbm", 200)
+            hybrid.set_model_index(model_map[site])
 
-            for timestamp in keys:
-                if (time_mapping[timestamp][0] == []):      # In case we don't have ground truth.
+            for timestamp in timestamps:
+                rssi = ground_truth_rssi = list()
+                start = acc = mag = gyro = ground_truth_imu = list()
+
+                if (str(timestamp) in time_to_rssi):
+                    rssi, ground_truth_rssi = time_to_rssi[str(timestamp)]
+
+                if (str(timestamp) in time_to_imu):
+                    start, acc, mag, gyro, ground_truth_imu = time_to_imu[str(timestamp)]
+
+                if (acc == [] or mag == [] or gyro == []):
                     continue
 
-                ground_truths.append(time_mapping[timestamp][0])
-                accelerator = time_mapping[timestamp][1][0]
-                gyroscope = time_mapping[timestamp][1][2]
-                magnometer = time_mapping[timestamp][1][1]
-                rssi = None
+                estimations.append(hybrid.next_position([timestamp, acc, mag, gyro], rssi))
 
-                if (len(time_mapping[timestamp]) < 3):
-                    rssi = []
+                if ground_truth_rssi != []:
+                    ground_truth.append(ground_truth_rssi)
 
-                else:
-                    rssi = time_mapping[timestamp][2]
-
-                pos = hybrid.next_position([int(timestamp), accelerator, magnometer, gyroscope], rssi)
-                estimations.append(pos)
+                elif ground_truth_imu != []:
+                    ground_truth.append(ground_truth_imu)
 
         site_count += 1
-        eval = evaluator.Evaluator(estimations, ground_truths)
+        eval = evaluator.Evaluator(estimations, ground_truth)
         print("MPE (site " + str(site_count) + "): " + str(eval.get_mpe()))
         print("RMSE (site " + str(site_count) + "): " + str(eval.get_rmse()) + "\n")
-        eo.write(eval, "../results/hybrid/average/" + site[0] + ".txt")
+        eo.write(eval, "../results/hybrid/pdr/" + site + ".txt")
