aba177fa8753f84b0bca6ee96f48bc1d445979ff
abiram98 <34130705+abiram98@users.noreply.github.com>
2021-05-26 21:37:53 +0200

    Neural network

--- ML/NeuralNet/models/model01.py
+++ ML/NeuralNet/models/model01.py
@@ -5,13 +5,9 @@ from tensorflow.keras import layers
 def create_model(input_d):
     w1 = int(input_d[0]/4)
     w2 = int(input_d[0]/2)
-    w3 = int(input_d[0])
     model = keras.Sequential()
-    model.add(layers.Dense(w3, activation='relu', input_shape=(input_d[1],)))
+    model.add(layers.Dense(w1, activation='relu', input_shape=(input_d[1],)))
     model.add(layers.Dense(w2, activation='relu'))
-    model.add(layers.Dense(w2, activation='relu'))
-    model.add(layers.Dense(w2, activation='relu'))
-    model.add(layers.Dense(w1, activation='relu'))
     model.add(layers.Dense(1))
     model.compile(optimizer=tf.optimizers.Adam(lr=0.001),
                   loss='mse', 
